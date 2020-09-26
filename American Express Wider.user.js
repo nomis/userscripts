e6e09b99300dd83f32b64368f23fcd12fcf43c94
Simon Arlott <sa.me.uk>
2020-09-26 09:52:18 +0100

    Update for new flex layout

--- American Express Wider.user.js
+++ American Express Wider.user.js
@@ -3,10 +3,15 @@
 // @version  1
 // @author   Simon Arlott
 // @license  GPL-3.0+
-// @include  https://global.americanexpress.com/*
+// @include  https://global.americanexpress.com/activity/*
 // @grant    none
 // ==/UserScript==
 
 var style = document.createElement('style');
-style.innerHTML = "div.statement-container, div.container { max-width: 1600px !important; }";
+style.setAttribute("type", "text/css");
+style.innerHTML = "\
+div.statement-container, div.container { max-width: 1600px !important; }\
+div[data-module-name=\"axp-activity-links\"], div[data-module-name=\"axp-activity-preview\"] { display: none !important; }\
+div[data-module-name=\"axp-activity-content\"] { flex-basis: 100% !important; max-width: 100% !important; }\
+";
 document.head.appendChild(style);
