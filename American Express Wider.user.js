8f1037cdfbfe213022c7d43d9497b7d08153a11e
Simon Arlott <sa.me.uk>
2021-08-30 15:22:45 +0100

    amex: Update for new names

--- American Express Wider.user.js
+++ American Express Wider.user.js
@@ -10,8 +10,8 @@
 var style = document.createElement('style');
 style.setAttribute("type", "text/css");
 style.innerHTML = "\
-div.statement-container, div.container { max-width: 1600px !important; }\
-div[data-module-name=\"axp-activity-links\"], div[data-module-name=\"axp-activity-preview\"] { display: none !important; }\
-div[data-module-name=\"axp-activity-content\"] { flex-basis: 100% !important; max-width: 100% !important; }\
+div.activity-container, div.statement-container, div.container { max-width: 1600px !important; }\
+div[data-module-name=\"axp-activity/Display/LinksContent\"] { display: none !important; }\
+div[data-module-name=\"axp-activity/Display/MainContent\"] { flex-basis: 100% !important; max-width: 100% !important; }\
 ";
 document.head.appendChild(style);
